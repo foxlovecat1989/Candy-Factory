507809ff57b29c60299d8ca498da03aa2acbe50b
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-10-06 02:47:53 +0800

    initial project

---
+++ candy-factory/src/main/java/com/candyfactory/CandyFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.candyfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandyFactoryApplication.class, args);
+	}
+
+}
---
+++ candy-factory/src/test/java/com/candyfactory/CandyFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candyfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandyFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
